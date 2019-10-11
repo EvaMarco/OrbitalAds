1c053d809de8580daf2cee76ad9e9b3b9bc91a19
Migara <evamarcod@gmail.com>
2019-10-11 10:35:57 +0200

    Add docs folder

---
+++ docs/precache-manifest.553e2facc049c60df2bb656fa713b92d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a009f79f15c3b35c2d01c9ebd7ab414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984f83deac5c84a029b1",
+    "url": "/static/css/main.32cd0289.chunk.css"
+  },
+  {
+    "revision": "2c4a131133d6969e2ee8",
+    "url": "/static/js/2.9353d6b7.chunk.js"
+  },
+  {
+    "revision": "984f83deac5c84a029b1",
+    "url": "/static/js/main.955e1af4.chunk.js"
+  },
+  {
+    "revision": "326702562ef1fd4697a3",
+    "url": "/static/js/runtime-main.777eca8c.js"
+  },
+  {
+    "revision": "ac4b8758349570a7dd4fe7e19dacfd5e",
+    "url": "/static/media/mountains.ac4b8758.png"
+  },
+  {
+    "revision": "1032cb92fa224eebe33da161e00126f6",
+    "url": "/static/media/search.1032cb92.svg"
+  }
+]);
